83c0d8ac152c10c65dffb1f9d9f38edc3b76e4a3
Andrey Grigoryev <i107th@gmail.com>
2016-01-24 13:53:17 +0500

    Add example of file writing in java

--- olymp/src/main/java/ru/sibint/olymp/api/APIEndpoint.java
+++ olymp/src/main/java/ru/sibint/olymp/api/APIEndpoint.java
@@ -1,9 +1,12 @@
 package ru.sibint.olymp.api;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -34,10 +37,10 @@ public class APIEndpoint {
 		}
 	}
 	
-	@Path("/submit/")
 	@POST
+	@Path("/submit/")
+	@Consumes("application/json")
 	@Produces(MediaType.TEXT_PLAIN)
-	@Consumes(MediaType.TEXT_PLAIN)
 	public String processSubmission(InputStream data) {
 		StringBuilder stringData = new StringBuilder();
 		try {
@@ -48,20 +51,31 @@ public class APIEndpoint {
 		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage());
 		}
+		
+		File F = new File("C:\\temp\\source.cpp");
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(F);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		pw.printf("%s", stringData.toString());
+		pw.flush();
+		
 		return "SUCCESS";
 	}
 	
-	@Path("/serverstatus/")
 	@GET
+	@Path("/serverstatus/")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getServerStatus() {
 		return "Server is up and runs application of version " + properties.getProperty("version");
 	}
 	
-	@Path("/adduser/")
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/adduser/")
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public String addUser(InputStream data) {
 		StringBuilder stringData = new StringBuilder();
 		try {
@@ -75,10 +89,10 @@ public class APIEndpoint {
 		return "SUCCESS";
 	}
 	
-	@Path("/addtask/")
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/addtask/")
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public String addTask(InputStream data) {
 		StringBuilder stringData = new StringBuilder();
 		try {
@@ -93,10 +107,10 @@ public class APIEndpoint {
 	}
 	
 	
-	@Path("/addtasktest/")
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/addtasktest/")
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public String addTaskTest(InputStream data) {
 		StringBuilder stringData = new StringBuilder();
 		try {
--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
 To tun REST web-service:
 1. Run it at server from eclipse
 2. go to the browser and check: http://localhost:8080/olymp/api/rest/serverstatus
+3. TO test POST method use curl utility (you could download it from http://curl.haxx.se/download.html) and next command:
+	curl -H "Content-Type: application/json" -X POST -d "{\"code\":\"123\"}" http://localhost:8080/olymp/api/rest/submit
