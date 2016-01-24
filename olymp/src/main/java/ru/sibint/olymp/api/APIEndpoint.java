87826f6d704d51d8f732b3f052b1fdb53234f9df
Andrey Grigoryev <i107th@gmail.com>
2016-01-24 12:11:39 +0500

    Create project and implement method getServerStatus to check if application is working

---
+++ olymp/src/main/java/ru/sibint/olymp/api/APIEndpoint.java
@@ -0,0 +1,45 @@
+package ru.sibint.olymp.api;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/rest/")
+public class APIEndpoint {
+
+	final static String version = "0.0.1";
+	
+	@Path("/submit/")
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String processSubmission(InputStream data) {
+		StringBuilder stringData = new StringBuilder();
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(data));
+			String line = "";
+			while((line = in.readLine()) != null)
+				stringData.append(line);
+		} catch (Exception e) {
+			Logger.getGlobal().log(Level.SEVERE, e.getMessage());
+		}
+		return stringData.toString();
+	}
+	
+	@Path("/serverstatus/")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getServerStatus() {
+		return "Server is up and runs application of version " + version;
+	}
+	
+}
--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
-начало положено
+To tun REST web-service:
+1. Run it at server from eclipse
+2. go to the browser and check: http://localhost:8080/olymp/api/rest/serverstatus
